d578afcbfa52bf20bbeb25f033839747fbae93e4
SunnyDay <1246771571@qq.com>
2022-06-16 22:13:47 +0800

    add DataBinding 监听器绑定

--- app/src/main/java/com/example/databingding/others/MyHandler.kt
+++ app/src/main/java/com/example/databingding/others/MyHandler.kt
@@ -5,7 +5,12 @@ import android.view.View
 
 class MyHandler {
     private val TAG = MyHandler::class.java.simpleName
-    fun onClickFriend(view: View,name:String) {
+    fun onClickFriend(view: View) {
         Log.d(TAG, "onClickFriend")
+    }
+
+    fun saveTask(taskName: String):String{
+        Log.d(TAG, "saveTask:$taskName")
+        return "aaa"
     }
 }
