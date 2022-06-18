9d3fb6d0098f7200a9436688b42b3344b8ef63e2
SunnyDay <1246771571@qq.com>
2022-06-18 13:34:44 +0800

    自定义可观察字段

--- app/src/main/java/com/example/databingding/ObservableFieldActivity.kt
+++ app/src/main/java/com/example/databingding/ObservableFieldActivity.kt
@@ -2,17 +2,38 @@ package com.example.databingding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.example.databingding.beans.Student
 import com.example.databingding.databinding.ActivityObservableFieldBinding
-import com.example.databingding.others.MyHandler
 
 class ObservableFieldActivity : AppCompatActivity() {
+    //绑定
+    private val binding: ActivityObservableFieldBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_observable_field)
+    }
+    private val stu1 = Student().apply {
+        stuName = "SunnyDay"
+    }
+    private val stu2 = Student().apply {
+        stuName = "Tom"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initVariableInXml()
+    }
 
-        val binding: ActivityObservableFieldBinding = DataBindingUtil.setContentView(this,R.layout.activity_observable_field)
-        binding.handler = MyHandler()
+    /**
+     * 对xml中的变量进行初始化值
+     * */
+    private fun initVariableInXml(){
+        binding.student = stu1
+        binding.activity = this
+    }
 
-        // 参考：https://www.jianshu.com/p/09d98b0b081b
+    fun changeText(view: View) {
+        stu1.stuName = "Kate"  // 不起作用
+       // binding.student = stu2
     }
 }
--- app/src/main/java/com/example/databingding/beans/Student.kt
+++ app/src/main/java/com/example/databingding/beans/Student.kt
@@ -1,5 +1,20 @@
 package com.example.databingding.beans
 
-data class Student(
-    val stuName: String
-)
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.example.databingding.BR
+
+
+class Student(): BaseObservable(){
+    @get:Bindable
+    var stuName = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.stuName)
+    }
+}
+//1、若不引入kapt插件，引入BaseObservable后编译报错。无法生成自定义类的BR字段。Unresolved reference: BR
+
+//2、BR包有两个，引入哪个都能跑起来。不知道有啥区别。
+// androidx.databinding.library.baseAdapters.BR
+// androidx.databinding.library.baseAdapters.BR
