b07014cb47e4f18a45e2d93aff9d9cd0dd1cf277
SunnyDay <1246771571@qq.com>
2022-06-25 12:53:37 +0800

    绑定适配器#自定义逻辑

--- app/src/main/java/com/example/databingding/extensions/Extensions.kt
+++ app/src/main/java/com/example/databingding/extensions/Extensions.kt
@@ -1,14 +1,30 @@
 package com.example.databingding.extensions
 
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.databinding.BindingAdapter
 import com.example.databingding.R
 
 /**
  * Create by SunnyDay 2022/06/24 19:43:11
  */
-@BindingAdapter("android:text")
-fun setTextPlus(view: TextView){
+class Extensions{}
+/**
+ * 自定义AppCompatEditText控件的hint方法逻辑。
+ *
+ * 当用户设置hint时直接走这里的setTextPlus方法，而不是走AppCompatEditText#setHit方法。
+ *
+ * */
+@BindingAdapter("android:hint")
+fun setTextPlus(view: AppCompatEditText,text:String){
+    view.hint = "input your name here"
     view.textSize = 18f
-    view.setTextColor(view.context.getColor(R.color.purple_500))
+    view.setHintTextColor(view.context.getColor(R.color.purple_500))
+}
+
+/**
+ * 为price属性设置自定义逻辑
+ * */
+@BindingAdapter("app:price")
+fun setPrice(view: AppCompatEditText,text:String){
+    view.hint = "input your price here!$text"
 }
